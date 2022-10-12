3075180d3846de7df50208545b63e60437da5234
Deependra Parmar <104254575+deependra051103@users.noreply.github.com>
2022-10-12 11:55:22 +0530

    Add files via upload

---
+++ Vaiables_and_Datatypes02.java
@@ -0,0 +1,20 @@
+public class Vaiables_and_Datatypes02 {
+    public static void main(String[] args) {
+//        Variables are the container that are memory allocations capable of storing data in the memory
+//        Datatype are basically identifiers that are responsible to identify the type of the data that is to be inserted
+//        in the variable and it is also responsible for the allocation of memory of the variable
+        int a = 8;
+        float b = (float) 9.90;
+        char c = 'a';
+
+        //The value which is being stored in the variable of any type is called as the Literal...
+
+//        now we are printing the values of the variables above.
+        System.out.println("Integer is: "+ a);
+        System.out.println("Float is: "+b);
+        System.out.println("Character is: " + c);
+
+
+
+    }
+}
